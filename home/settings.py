80c159c97cf12d1cd2c5af3f60deb48993e56f7b
Maka <ibrahimismailadeniyi9@gmail.com>
2022-08-01 21:42:30 +0800

    first commit

--- home/settings.py
+++ home/settings.py
@@ -28,6 +28,8 @@ DEBUG = False
 ALLOWED_HOSTS = [ 'facebook91.herokuapp.com', '127.0.0.1' ]
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -116,7 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
